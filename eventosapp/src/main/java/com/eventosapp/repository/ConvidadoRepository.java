ae1d7d5a5480073bf2222db35d444246cff4a803
Eduardo Lima <eduardoaluno1800@gmail.com>
2025-07-24 18:47:20 -0300

    Adicionando detalhamento e dados dentro dos Eventos

--- eventosapp/src/main/java/com/eventosapp/repository/ConvidadoRepository.java
+++ eventosapp/src/main/java/com/eventosapp/repository/ConvidadoRepository.java
@@ -3,8 +3,9 @@ package com.eventosapp.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.eventosapp.models.Convidado;
+import com.eventosapp.models.Evento;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String>{
-
+	Iterable<Convidado> findByEvento(Evento evento);
 	
 }
