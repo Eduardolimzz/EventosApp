c7778920109b9022147001f5fcd41ab6350b0eae
Eduardo Lima <eduardoaluno1800@gmail.com>
2025-07-24 19:37:46 -0300

    Adicionando verificações de inserção para formularios

--- eventosapp/src/main/java/com/eventosapp/models/Convidado.java
+++ eventosapp/src/main/java/com/eventosapp/models/Convidado.java
@@ -3,36 +3,43 @@ package com.eventosapp.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Convidado {
-	
-	@Id
-	private String rg;
-	private String nomeConvidado;
-	
-	@ManyToOne
-	private Evento evento;
-	
-	public String getRg() {
-		return rg;
-	}
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-	public String getNomeConvidado() {
-		return nomeConvidado;
-	}
-	public void setNomeConvidado(String nomeConvidado) {
-		this.nomeConvidado = nomeConvidado;
-	}
-	public Evento getEvento() {
-		return evento;
-	}
-	public void setEvento(Evento evento) {
-		this.evento = evento;
-	}
-	
-	
-	
+
+    @Id
+    @NotBlank(message = "RG é obrigatório")
+    private String rg;
+
+    @NotBlank(message = "Nome do convidado é obrigatório")
+    private String nomeConvidado;
+
+    @ManyToOne
+    private Evento evento;
+
+    // Getters e Setters
+    public String getRg() {
+        return rg;
+    }
+    
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
+    public String getNomeConvidado() {
+        return nomeConvidado;
+    }
+    
+    public void setNomeConvidado(String nomeConvidado) {
+        this.nomeConvidado = nomeConvidado;
+    }
+    
+    public Evento getEvento() {
+        return evento;
+    }
+    
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
 }
